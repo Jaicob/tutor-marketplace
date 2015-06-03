e3f369de0feaf0c72e46de7c2bbf6cca3e8e57e5
anurag-banerjee <anuragb2010@gmail.com>
2015-06-03 15:53:29 -0400

    dynamically change the dashbar by the user's account type
    - doesn’t support admin currently

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,3 @@
 module ApplicationHelper
-
-	def show_dashboard_nav?
-		@dashboard_links = {
-			# name seen on the dashboard nav => where it links to
-			"Dashboard" => "/dashboard",
-			"Schedule"  => "/dashboard/schedule",
-			"Courses"   => "/dashboard/courses",
-			"Profile"   => "/dashboard/profile",
-			"Settings"  => "/dashboard/settings"
-		}
-
-		@dashboard_links.each do |name, link|
-			@current = name.downcase if request.fullpath.start_with? link
-		end
-
-		# check if current page is a dashboard page and if signed in
-		@current && user_signed_in?
-	end
-
+	include DashboardNavHelper
 end
