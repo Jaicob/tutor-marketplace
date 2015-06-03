f6eb4933d73bcbe4c5c7cda53054bf78f7841c4c
anurag-banerjee <anuragb2010@gmail.com>
2015-06-03 15:00:27 -0400

    make the dashboard tabs route to dummy pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,23 @@
 class DashboardController < ApplicationController
 
 	def index
+		@content = "dashboard page is here"
+	end
+
+	def schedule
+		@content = "schedule page is here"
+	end
+
+	def courses
+		@content = "courses page is here"
+	end
+
+	def profile
+		@content = "profile page is here"
+	end
+
+	def settings
+		@content = "settings page is here"
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   resources :courses
   resources :subjects
   resources :schools
-  resources :dashboard
+  resources :dashboard do
+    collection do
+      get '/schedule' => 'dashboard#schedule'
+      get '/courses' => 'dashboard#courses'
+      get '/profile' => 'dashboard#profile'
+      get '/settings' => 'dashboard#settings'
+    end
+  end
   devise_for :users
   mount Upmin::Engine => '/admin'
   root to: "static_pages#home"
+
 end
