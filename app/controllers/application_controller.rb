bd48794adf48b106a5d0362fe479803f9bea7172
JT Jobe <jtjobe@gmail.com>
2015-06-05 16:29:07 -0400

    Changed landing page after Devise login to Courses page of Dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,19 +11,19 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
-      if resource.tutor 
-        dashboard_placeholder_tutor_path(resource.tutor)
+      if resource.tutor
+        dashboard_courses_tutor_path(resource.tutor)
       else
         root_path
       end
     end
 
     def after_sign_up_path_for(resource)
-      dashboard_placeholder_tutor_path(resource.tutor)
+      dashboard_courses_tutor_path(resource.tutor)
     end
 
     def after_inactive_sign_up_path_for(resource)
-      dashboard_placeholder_tutor_path(resource.tutor)
+      dashboard_courses_tutor_path(resource.tutor)
     end
 
 end
