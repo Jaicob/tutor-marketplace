96ae593938defaaaace2cf65971a478a303ee7b7
JT <jtjobe@gmail.com>
2015-06-09 18:00:09 -0400

    Git stash disaster?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery.jcrop
+//= require papercrop
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
