872d012e295f668c117321af0b13ef871aa17616
JT <jtjobe@gmail.com>
2015-06-15 11:03:56 -0400

    Fixed data type for User first_name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612212142) do
+ActiveRecord::Schema.define(version: 20150615150146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 20150612212142) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
-    t.text     "first_name"
+    t.string   "first_name"
     t.string   "last_name"
     t.string   "slug"
   end
