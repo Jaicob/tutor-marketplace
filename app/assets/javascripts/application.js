0adf18854b15f2d7e6e888bce486467fda67ce0b
anurag-banerjee <anuragb2010@gmail.com>
2015-06-17 20:51:33 -0400

    react installation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery_ujs
 //= require jquery.Jcrop.min
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require foundation
 //= require_tree .
 $(function() {
