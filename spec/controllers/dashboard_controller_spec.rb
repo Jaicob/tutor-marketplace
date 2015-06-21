62f65e1309c2ba98913d36eeb8560d45b7a79486
JT <jtjobe@gmail.com>
2015-06-20 18:03:55 -0600

    Finished controller tests for Dashboard

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -4,10 +4,6 @@ describe DashboardController do
   let(:user) { create(:user) }
   let(:tutor) { create(:complete_tutor) }
 
-  # before :each do
-  #   login_with user
-  # end
-
   before :each do |example|
     unless example.metadata[:skip_before]
       login_with user
@@ -68,7 +64,7 @@ describe DashboardController do
 
   describe 'PUT #update_profile' do
 
-    it "updates a tutors's profile settings", :skip_before do
+    it "updates a tutors's profile", :skip_before do
       login_with tutor.user
       put :update_profile, id: tutor.user, data: {tutor: {major: 'Astrology'}}
       expect(response).to redirect_to profile_user_path(tutor.user)
@@ -78,9 +74,11 @@ describe DashboardController do
   end
 
   describe 'PUT #change_profile_pic' do
+    skip 'not sure how to test this yet'
   end
 
-  describe 'PUT #save_profile_pic' do 
+  describe 'PUT #save_profile_pic_crop' do
+    skip 'not sure how to test this yet' 
   end
 
   describe 'GET #settings' do 
@@ -96,7 +94,45 @@ describe DashboardController do
     end 
   end
 
-  describe 'PUT #update_settings' do 
+  describe 'PUT #update_settings' do
+
+    it "updates a user's settings" do
+      put :update_settings, id: user, data: {user: {first_name: 'Ludacris'}}
+      expect(response).to redirect_to settings_user_path(user)
+      user.reload
+      expect(user.first_name).to eq('Ludacris')
+    end 
+
+     it "updates a tutors's profile", :skip_before do
+      login_with tutor.user
+      put :update_profile, id: tutor.user, data: {tutor: {major: 'Astrology'}}
+      expect(response).to redirect_to profile_user_path(tutor.user)
+      tutor.reload
+      expect(tutor.major).to eq('Astrology')
+    end
+
+    it "updates a tutors's settings on user model", :skip_before do
+      login_with tutor.user
+      put :update_settings, id: tutor.user, data: {
+        user: {last_name: 'Ricardo'},
+        tutor: {phone_number: tutor.phone_number} # No change here, but need some tutor data to make action work
+      }
+      expect(response).to redirect_to settings_user_path(tutor.user)
+      tutor.reload
+      expect(tutor.user.last_name).to eq('Ricardo')
+    end
+
+    it "updates a tutors's settings on tutor model", :skip_before do
+      login_with tutor.user
+      put :update_settings, id: tutor.user, data: {
+          user: {first_name: tutor.user.first_name }, # No change here, but need some user data to make action work
+          tutor: {phone_number: '0000000000'}
+      }
+      expect(response).to redirect_to settings_user_path(tutor.user)
+      tutor.reload
+      expect(tutor.phone_number).to eq('0000000000')
+    end
+
   end
 
 end
