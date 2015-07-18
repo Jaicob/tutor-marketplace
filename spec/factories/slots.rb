5dd370c7d03a89cd7c3b0687c7b2c82f892a3897
Jaicob <jaicob@icloud.com>
2015-07-18 19:55:43 -0400

    adjust factory for slot

--- spec/factories/slots.rb
+++ spec/factories/slots.rb
@@ -6,8 +6,8 @@ FactoryGirl.define do
     end_time "2015-08-14 13:00:00"
     reservation_min 1
     reservation_max 4
-    start_week_time "Fri 12:00:00"
-    end_week_time "Fri 13:00:00"
+    start_week_time "Fri 12:00:00" # Normally Calculated
+    end_week_time "Fri 13:00:00" # Normally Calculated
 
     factory :one_slot do 
       start_date  "2015-08-14"
