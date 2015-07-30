f66c3c0a71464dd83a180481a4510672266b3d23
Anurag Banerjee <anuragb2010@gmail.com>
2015-07-29 23:51:04 -0400

    add tutor search link to homepage and dashbar

--- app/controllers/single_views_controller.rb
+++ app/controllers/single_views_controller.rb
@@ -2,6 +2,7 @@ class SingleViewsController < ApplicationController
 
   def home
     @sign_up_path = if user_signed_in? then '/tutors/new' else '/tutors/visitor_new' end
+	@search_path = search_path()
   end
 
 end
