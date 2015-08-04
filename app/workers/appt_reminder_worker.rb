ad12b20f590de1d832a1c7cb796d73f381a6cd3a
JT <jtjobe@gmail.com>
2015-08-04 15:36:01 -0400

    Added comments

--- app/workers/appt_reminder_worker.rb
+++ app/workers/appt_reminder_worker.rb
@@ -1,6 +1,11 @@
 class ApptReminderWorker
   include Sidekiq::Worker 
 
+  # This workers handles the appointment reminder emails. It only 
+  # sends a reminder email if the appt_reminder_email_date on an appointment
+  # matches the current date, which effectively disables any out-of-date 
+  # reminders from being sent if the appointment date was changed.
+
   def perform(appointment_id)
     @appt = Appointment.find(appointment_id)
     @date = Time.at(@appt.appt_reminder_email_date).to_date
