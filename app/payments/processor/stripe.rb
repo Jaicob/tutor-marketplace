8f20653b9c5e75f2ef423653d798e45126a57abb
Jordan Burke <me@jordankburke.com>
2015-08-10 23:31:12 -0400

    Updated create tutor method

--- app/payments/processor/stripe.rb
+++ app/payments/processor/stripe.rb
@@ -1,5 +1,38 @@
+require 'stripe'
+
 module Payment
   class Stripe
+
+    def initialize(api_key)
+      Stripe.api_key = api_key
+    end
+
+    def create_managed_account(tutor)
+      Stripe::Account.create(
+        managed: true,
+        country: 'US',
+        email: tutor.email,
+        legal_entity: {
+          type: "individual",
+          address: {
+            line1: tutor.line1,
+            line2: tutor.line2,
+            city: tutor.city,
+            state: tutor.state,
+            postal_code: tutor.postal_code
+          },
+          first_name: tutor.first_name,
+          last_name: tutor.last_name,
+          dob: tutor.dob
+          ssn_last_4: tutor.ssn_last_4,
+        },
+        debit_negative_balances: true,
+        tos_acceptance: {
+          date: Time.zone.now,
+          ip: tutor.ip
+        }
+      )
+    end
 
   end
 end
--- app/services/payment/create_tutor_stripe.rb
+++ app/services/payment/create_tutor_stripe.rb
@@ -1,4 +1,9 @@
-class CreateTutorStripe
-  include Interactor
+class CreateTutorAccount
+
+  def build(tutor)
+    stripe = Payment::Stripe.new
+    response = stripe.create_managed_account(tutor)
+    tutor.update_attributes(payment_acct: response[:id])
+  end
 
 end
