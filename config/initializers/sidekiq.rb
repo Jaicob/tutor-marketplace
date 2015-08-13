b50a98878e7cf239d801ba8f317de89e9e3586fd
JT <jtjobe@gmail.com>
2015-08-13 17:40:22 -0400

    Created new separate container for Redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,20 @@
-# This file isn't needed in development, but some configuration will go here in production
-#
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://dockerhost:6379' }
-# end
+redis_domain = ENV['WEBAPP_REDIS_1_PORT_6379_TCP_ADDR']
+redis_port = ENV['WEBAPP_REDIS_1_PORT_6379_TCP_PORT']
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://localhost:6379' }
-# end
+if redis_domain && redis_port
+  redis_url = "redis://#{redis_domain}:#{redis_port}"
+
+  Sidekiq.configure_server do |config|
+    config.redis = { 
+      namespace: "sidekiq",
+      url: redis_url
+    }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      namespace: "sidekiq",
+      url: redis_url
+    }
+  end
+end
