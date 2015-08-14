5cfd5befdc9e2d3c345f4fb7b65ad5f7b29ef5e7
Anurag Banerjee <anuragb2010@gmail.com>
2015-08-14 14:14:54 -0400

    fix edge case where course selector doesn't load correctly after returning from another page

--- app/assets/javascripts/single_views/search.js
+++ app/assets/javascripts/single_views/search.js
@@ -4,12 +4,20 @@ $(document).ready(function(){
 		var courseId = $("#course").val();
 		if ($.isNumeric(schoolId) && $.isNumeric(courseId)) {
 			React.render(
-			    React.createElement(TutorCardContainer, {school: schoolId, course: courseId}),
+			    React.createElement(TutorCardContainer, {
+			    	school: schoolId,
+			    	course: courseId
+			    }),
 			    document.querySelector('#tutorCardContainer')
 			);
 		} else {
 			swal("Uh-oh", "Please select a school and a course.", "error");
 		}
 	}
+
+	$("#school")[0].selectedIndex = 0;
+	$("#subject")[0].selectedIndex = 0;
+	$("#course")[0].selectedIndex = 0;
+
 	$("#tutorSearchButton").on("click", updateReactComponents);
 });
