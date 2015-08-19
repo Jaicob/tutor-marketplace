d8b5af4dfbc11d7a20ee2d8ad1f1980c8dad43bd
JT <jtjobe@gmail.com>
2015-08-19 17:44:01 -0400

    Changed user_id to tutor_id on tutor mailers to accomodate sign-up without account

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -23,7 +23,7 @@ class TutorsController < ApplicationController
     if @tutor.save
       # The method below only creates a tutor_course for the initial sign-up, all other CRUD operations relating to tutor_courses go through the TutorCoursesController
       @tutor.set_first_tutor_course(@tutor, params)
-      TutorManagementMailer.delay.welcome_email(@tutor.user.id)
+      TutorManagementMailer.delay.welcome_email(@tutor.id)
       redirect_to dashboard_home_user_path(current_user)
     else
       flash[:alert] = "Tutor account was not created. Please fill in all fields and attach your unofficial transcript."
@@ -84,6 +84,7 @@ class TutorsController < ApplicationController
     if @tutor.save
       # The method below only creates a tutor_course for the initial sign-up, all other CRUD operations relating to tutor_courses go through the TutorCoursesController
       @tutor.set_first_tutor_course(@tutor, params)
+      TutorManagementMailer.delay_for(5.minutes).welcome_email(@tutor.id)
       redirect_to register_or_sign_in_tutor_path(@tutor)
     else
       flash[:error] = "Tutor account was not created. Please fill in all fields and attach your unofficial transcript."
--- app/mailers/tutor_management_mailer.rb
+++ app/mailers/tutor_management_mailer.rb
@@ -2,28 +2,33 @@ class TutorManagementMailer < ApplicationMailer
   default from: 'info@axontutors.com'
   default template_path: "mailers/#{self.name.underscore}"
 
-  def welcome_email(user_id)
-    @user = User.find(user_id)
+  def welcome_email(tutor_id)
+    @tutor = Tutor.find(tutor_id)
+    @user = @tutor.user
     mail(to: @user.email, subject: "Thanks for signing up to become an Axon Tutor!")
   end
 
-  def application_completed_email(user_id)
-    @user = User.find(user_id)
+  def application_completed_email(tutor_id)
+    @tutor = Tutor.find(tutor_id)
+    @user = @tutor.user
     mail(to: @user.email, subject: "Way to go! You've completed your application to become an Axon Tutor")
   end
 
-  def rejection_email(user_id)
-    @user = User.find(user_id)
+  def rejection_email(tutor_id)
+    @tutor = Tutor.find(tutor_id)
+    @user = @tutor.user
     mail(to: @user.email, subject: "Regarding your recent application to become an Axon Tutor")
   end
 
-  def activation_email(user_id)
-    @user = User.find(user_id)
+  def activation_email(tutor_id)
+    @tutor = Tutor.find(tutor_id)
+    @user = @tutor.user
     mail(to: @user.email, subject: "Congratulations! Your Axon Tutors account has been activated!")
   end
 
   def deactivation_email(user_id)
-    @user = User.find(user_id)
+    @tutor = Tutor.find(tutor_id)
+    @user = @tutor.user
     mail(to: @user.email, subject: "Your Axon Tutors account has been de-activated")
   end
 
