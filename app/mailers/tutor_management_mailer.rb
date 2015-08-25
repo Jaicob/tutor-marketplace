6f819285c4d8d0e04cc434d53d16a2b9318335d5
JT <jtjobe@gmail.com>
2015-08-25 11:18:01 -0400

    Removed exclamation marks from subject lines

--- app/mailers/tutor_management_mailer.rb
+++ app/mailers/tutor_management_mailer.rb
@@ -6,14 +6,14 @@ class TutorManagementMailer < ApplicationMailer
     @user = User.find(user_id)
     @tutor = @user.tutor
     @user = @tutor.user
-    mail(to: @user.email, subject: "Thanks for signing up to become an Axon Tutor!")
+    mail(to: @user.email, subject: "Thanks for signing up to become an Axon Tutor")
   end
 
   def application_completed_email(user_id)
     @user = User.find(user_id)
     @tutor = @user.tutor
     @user = @tutor.user
-    mail(to: @user.email, subject: "Way to go! You've completed your application to become an Axon Tutor")
+    mail(to: @user.email, subject: "You've completed your application to become an Axon Tutor")
   end
 
   def rejection_email(user_id)
@@ -27,7 +27,7 @@ class TutorManagementMailer < ApplicationMailer
     @user = User.find(user_id)
     @tutor = @user.tutor
     @user = @tutor.user
-    mail(to: @user.email, subject: "Your Axon Tutors account has been activated!")
+    mail(to: @user.email, subject: "Your Axon Tutors account has been activated")
   end
 
   def deactivation_email(user_id)
