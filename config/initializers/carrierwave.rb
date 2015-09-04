d124bde2cf9ed5c2bb2aab6cc31a80725332892e
Jaicob <jaicob@icloud.com>
2015-09-04 17:23:13 -0400

    remove unecessary code

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,21 +8,14 @@ if Rails.env.test? or Rails.env.cucumber?
   end
 else
   CarrierWave.configure do |config|
-    # config.storage = :fog
-    # config.enable_processing = false
-                      
-  	config.fog_credentials = {
-    	provider:              'AWS',                      
-    	aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                     
-    	aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                  
-    	region:                ENV['AWS_REGION'],                 
-    	# host:                  ENV['AWS_HOST'],       
-    	# endpoint:              ENV['AWS_ENDPOINT'] 
-  	}
-  	config.fog_directory  = 'rails-axon-uploads' 
-    # config.fog_host       = ENV['AWS_HOST'],                    
-  	config.fog_public     = true                                        
-  	config.fog_attributes = { 'Cache-Control' => "max-age=#{2.day.to_i}" } 
-  end
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = 'rails-axon-uploads'
+    config.fog_public     = true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{2.day.to_i}" }
+  end  
 end
-
