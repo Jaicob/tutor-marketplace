2c6ff66abc44e1f5a26efb79a00a08200b421abe
JT <jtjobe@gmail.com>
2015-09-14 10:19:50 -0400

    Annotated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -138,7 +138,6 @@
 #                        api_v1_tutor_slots_update POST     /api/v1/tutors/:tutor_id/slots/update(.:format)                    api/v1/slots#update_slots {:format=>:json}
 #                  cancel_api_v1_tutor_appointment PUT      /api/v1/tutors/:tutor_id/appointments/:id/cancel(.:format)         api/v1/tutor_appointments#cancel {:format=>:json}
 #                        api_v1_tutor_appointments GET      /api/v1/tutors/:tutor_id/appointments(.:format)                    api/v1/tutor_appointments#index {:format=>:json}
-#                                                  POST     /api/v1/tutors/:tutor_id/appointments(.:format)                    api/v1/tutor_appointments#create {:format=>:json}
 #                         api_v1_tutor_appointment GET      /api/v1/tutors/:tutor_id/appointments/:id(.:format)                api/v1/tutor_appointments#show {:format=>:json}
 #            reschedule_api_v1_student_appointment PUT      /api/v1/students/:student_id/appointments/:id/reschedule(.:format) api/v1/student_appointments#reschedule {:format=>:json}
 #                cancel_api_v1_student_appointment PUT      /api/v1/students/:student_id/appointments/:id/cancel(.:format)     api/v1/student_appointments#cancel {:format=>:json}
