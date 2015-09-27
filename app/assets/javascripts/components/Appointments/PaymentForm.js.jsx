4247a4586518b441732ad53188034fba86a8cc2b
Jordan Burke <me@jordankburke.com>
2015-09-27 17:26:50 -0400

    Added browserify-rails and package.json

--- app/assets/javascripts/components/Appointments/PaymentForm.js.jsx
+++ app/assets/javascripts/components/Appointments/PaymentForm.js.jsx
@@ -1,8 +1,71 @@
-var PaymentForm = React.createClass({
-  render: function () {
+var ReactScriptLoaderMixin = require('react-script-loader').ReactScriptLoaderMixin;
+
+var StripeButton = React.createClass({
+  mixins: [ReactScriptLoaderMixin],
+  getScriptURL: function() {
+    return 'https://checkout.stripe.com/checkout.js';
+  },
+
+  statics: {
+    stripeHandler: null,
+    scriptDidError: false,
+  },
+
+  // Indicates if the user has clicked on the button before the
+  // the script has loaded.
+  hasPendingClick: false,
+
+  onScriptLoaded: function() {
+    // Initialize the Stripe handler on the first onScriptLoaded call.
+    // This handler is shared by all StripeButtons on the page.
+    if (!StripeButton.stripeHandler) {
+      StripeButton.stripeHandler = StripeCheckout.configure({
+        key: 'YOUR_STRIPE_KEY',
+        image: '/YOUR_LOGO_IMAGE.png',
+        token: function(token) {
+        // Use the token to create the charge with a server-side script.
+        }
+      });
+      if (this.hasPendingClick) {
+        this.showStripeDialog();
+      }
+    }
+  },
+  showLoadingDialog: function() {
+  // show a loading dialog
+  },
+  hideLoadingDialog: function() {
+  // hide the loading dialog
+  },
+  showStripeDialog: function() {
+    this.hideLoadingDialog();
+    StripeButton.stripeHandler.open({
+      name: 'Demo Site',
+      description: '2 widgets ($20.00)',
+      amount: 2000
+    });
+  },
+  onScriptError: function() {
+    this.hideLoadingDialog();
+    StripeButton.scriptDidError = true;
+  },
+  onClick: function() {
+    if (StripeButton.scriptDidError) {
+      console.log('failed to load script');
+    } else if (StripeButton.stripeHandler) {
+      this.showStripeDialog();
+    } else {
+      this.showLoadingDialog();
+      this.hasPendingClick = true;
+    }
+  },
+  render: function() {
     return (
       <div>
+        <button onClick={this.onClick}>Book Appointments</button>
       </div>
-    )
+    );
   }
 });
+
+exports.StripeButton = StripeButton;
--- config/application.rb
+++ config/application.rb
@@ -82,5 +82,41 @@ module WebApp
     #   optional: ["transformerName"],  # pass extra babel options
     #   whitelist: ["useStrict"] # even more options
     # }
+    # Paths, that should be browserified. We browserify everything, that
+    # matches (===) one of the paths. So you will most likely put lambdas
+    # regexes in here.
+    #
+    # By default only files in /app and /node_modules are browserified,
+    # vendor stuff is normally not made for browserification and may stop
+    # working.
+    # config.browserify_rails.paths << /vendor\/assets\/javascripts\/module\.js/
+
+    # Environments, in which to generate source maps
+    #
+    # The default is none
+    config.browserify_rails.source_map_environments << "development"
+
+    # Should the node_modules directory be evaluated for changes on page load
+    #
+    # The default is `false`
+    config.browserify_rails.evaluate_node_modules = true
+
+    # Force browserify on every found JavaScript asset
+    #
+    # The default is `false`
+    # config.browserify_rails.force = true
+
+    # Command line options used when running browserify
+    #
+    # can be provided as an array:
+    # config.browserify_rails.commandline_options = ["-t browserify-shim", "--fast"]
+
+    # or as a string:
+    config.browserify_rails.commandline_options = "-t browserify-shim --fast"
+
+    # Define NODE_ENV to be used with envify
+    #
+    # defaults to Rails.env
+    # config.browserify_rails.node_env = "production"
   end
 end
