1effcc7f4f98cc35023fe2efc0169d69d3ea8ef8
JT <jtjobe@gmail.com>
2015-09-30 09:53:59 -0400

    Finished tests for repeating dollar-off promo

--- spec/services/promo_code_helpers/repeating_dollar_amount_off_from_tutor_spec.rb
+++ spec/services/promo_code_helpers/repeating_dollar_amount_off_from_tutor_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'PromoCodeHelpers::RepeatingDollarAmountOffFromTutor' do
       tutor_course = create(:tutor_course, tutor_id: @tutor.id, course_id: course_id )
     end
 
-    it 'adjusts fees for a $5-off coupon issued by a Tutor for one eligible appt' do 
+    it 'adjusts fees for a $5-off repeating-tutor-discount for one eligible appt' do 
       @promotion.update(amount: 5)
       params = {
         tutor: @tutor,
@@ -42,7 +42,35 @@ RSpec.describe 'PromoCodeHelpers::RepeatingDollarAmountOffFromTutor' do
       expect(@context.charge.axon_fee).to eq 270
     end
 
-    it 'does not adjust fees for a $5-off coupon issued by a Tutor for a non-eligible appt' do 
+    it 'adjusts fees for a $5-off repeating-tutor-discount for two eligible appts' do 
+      @promotion.update(amount: 5)
+      new_slot_id = @tutor.slots.create(start_time: '2015-09-20 10:00:00', duration: 3600).id
+      course_id = @promotion.course_id
+      @appointment_two = create(:appointment, slot_id: new_slot_id, course_id: course_id, start_time: '2015-09-20 10:00:00')
+      params = {
+        tutor: @tutor,
+        appointments: [@appointment, @appointment_two],
+        customer_id: 1,
+        token: 1111111111,
+        rates: [23, 23],
+        transaction_percentage: 15,
+        promotion_id: @promotion.id,
+        is_payment_required: true,
+        promotion_category: nil
+      }
+      context = CreateCharge.call(params)
+      @context = PromoCodeHelpers::RepeatingDollarAmountOffFromTutor.new(context)
+
+      expect(@context.charge.amount).to eq 5290
+      expect(@context.charge.tutor_fee).to eq 4600
+      expect(@context.charge.axon_fee).to eq 690
+      @context.return_adjusted_fees
+      expect(@context.charge.amount).to eq 4140
+      expect(@context.charge.tutor_fee).to eq 3600
+      expect(@context.charge.axon_fee).to eq 540
+    end
+
+    it 'does not adjust fees for a $5-off repeating-tutor-discount for a non-eligible appt' do 
       @promotion.update(amount: 5)
       @appointment.update(course_id: second_course.id)
       params = {
@@ -68,150 +96,33 @@ RSpec.describe 'PromoCodeHelpers::RepeatingDollarAmountOffFromTutor' do
       expect(@context.charge.axon_fee).to eq 345
     end
 
-    # it 'correctly adjusts fees for a $10-off coupon issued by a Tutor' do 
-    #   @promotion.update(amount: 10)
-    #   params = {
-    #     tutor: tutor,
-    #     appointments: [appointment],
-    #     customer_id: 1,
-    #     token: 1111111111,
-    #     rates: [23],
-    #     transaction_percentage: 15,
-    #     promotion_id: @promotion.id,
-    #     is_payment_required: true,
-    #     promotion_category: nil
-    #   }
-    #   context = CreateCharge.call(params)
-    #   @context = PromoCodeHelpers::DollarAmountOffFromTutor.new(context)
+    it 'only adjust fees for eligible appts with a $5-off repeating-tutor-discount on a booking with both eligible and non-eligible appts' do 
+      @promotion.update(amount: 5)
+      new_slot_id = @tutor.slots.create(start_time: '2015-09-20 10:00:00', duration: 3600).id
+      course_id = @promotion.course_id
+      @appointment_two = create(:appointment, slot_id: new_slot_id, course_id: second_course.id, start_time: '2015-09-20 10:00:00')
+      params = {
+        tutor: @tutor,
+        appointments: [@appointment, @appointment_two],
+        customer_id: 1,
+        token: 1111111111,
+        rates: [23, 23],
+        transaction_percentage: 15,
+        promotion_id: @promotion.id,
+        is_payment_required: true,
+        promotion_category: nil
+      }
+      context = CreateCharge.call(params)
+      @context = PromoCodeHelpers::RepeatingDollarAmountOffFromTutor.new(context)
 
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    #   @context.return_adjusted_fees
-    #   expect(@context.charge.amount).to eq 1495
-    #   expect(@context.charge.tutor_fee).to eq 1300
-    #   expect(@context.charge.axon_fee).to eq 195
-    # end
-
-    # it 'correctly adjusts fees for a $20-off coupon issued by a Tutor' do 
-    #   @promotion.update(amount: 20)
-    #   params = {
-    #     tutor: tutor,
-    #     appointments: [appointment],
-    #     customer_id: 1,
-    #     token: 1111111111,
-    #     rates: [23],
-    #     transaction_percentage: 15,
-    #     promotion_id: @promotion.id,
-    #     is_payment_required: true,
-    #     promotion_category: nil
-    #   }
-    #   context = CreateCharge.call(params)
-    #   @context = PromoCodeHelpers::DollarAmountOffFromTutor.new(context)
-
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    #   @context.return_adjusted_fees
-    #   expect(@context.charge.amount).to eq 345
-    #   expect(@context.charge.tutor_fee).to eq 300
-    #   expect(@context.charge.axon_fee).to eq 45
-    # end
-
-    # it 'increments the redemption_count for a promotion by 1 when succesfully applied' do
-    #   params = {
-    #     tutor: tutor,
-    #     appointments: [appointment],
-    #     customer_id: 1,
-    #     token: 1111111111,
-    #     rates: [23],
-    #     transaction_percentage: 15,
-    #     promotion_id: @promotion.id,
-    #     is_payment_required: true,
-    #     promotion_category: nil
-    #   }
-    #   context = CreateCharge.call(params)
-    #   @context = PromoCodeHelpers::DollarAmountOffFromTutor.new(context)
-
-    #   expect(@promotion.redemption_count).to eq 0
-    #   @context.return_adjusted_fees
-    #   expect(@promotion.reload.redemption_count).to eq 1
-    # end
-
-    # it 'does not give discount for promo_code if code is past redemption_limit' do 
-    #   @promotion.update(redemption_limit: 100, redemption_count: 100)
-    #   params = {
-    #     tutor: tutor,
-    #     appointments: [appointment],
-    #     customer_id: 1,
-    #     token: 1111111111,
-    #     rates: [23],
-    #     transaction_percentage: 15,
-    #     promotion_id: @promotion.id,
-    #     is_payment_required: true,
-    #     promotion_category: nil
-    #   }
-    #   context = CreateCharge.call(params)
-    #   @context = PromoCodeHelpers::DollarAmountOffFromTutor.new(context)
-
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    #   @context.return_adjusted_fees
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    # end
-
-    #  it 'does not give discount for expired promo_code' do
-    #   @promotion.update(valid_from: Date.today - 300, valid_until: Date.today - 299) 
-    #   params = {
-    #     tutor: tutor,
-    #     appointments: [appointment],
-    #     customer_id: 1,
-    #     token: 1111111111,
-    #     rates: [23],
-    #     transaction_percentage: 15,
-    #     promotion_id: @promotion.id,
-    #     is_payment_required: true,
-    #     promotion_category: nil
-    #   }
-    #   context = CreateCharge.call(params)
-    #   @context = PromoCodeHelpers::DollarAmountOffFromTutor.new(context)
-
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    #   @context.return_adjusted_fees
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    # end
-
-    # it 'does not give discount if tutor on promo_code and charge are not the same' do 
-    #   @promotion.update(tutor_id: different_tutor.id) 
-    #   params = {
-    #     tutor: tutor,
-    #     appointments: [appointment],
-    #     customer_id: 1,
-    #     token: 1111111111,
-    #     rates: [23],
-    #     transaction_percentage: 15,
-    #     promotion_id: @promotion.id,
-    #     is_payment_required: true,
-    #     promotion_category: nil
-    #   }
-    #   context = CreateCharge.call(params)
-    #   @context = PromoCodeHelpers::DollarAmountOffFromTutor.new(context)
-
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    #   @context.return_adjusted_fees
-    #   expect(@context.charge.amount).to eq 2645
-    #   expect(@context.charge.tutor_fee).to eq 2300
-    #   expect(@context.charge.axon_fee).to eq 345
-    # end
+      expect(@context.charge.amount).to eq 5290
+      expect(@context.charge.tutor_fee).to eq 4600
+      expect(@context.charge.axon_fee).to eq 690
+      @context.return_adjusted_fees
+      expect(@context.charge.amount).to eq 4715
+      expect(@context.charge.tutor_fee).to eq 4100
+      expect(@context.charge.axon_fee).to eq 615
+    end
 
   end
 end
