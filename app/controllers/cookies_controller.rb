41dabb4dc2f33ade72a97af716d00d6e18ed2ea5
JT <jtjobe@gmail.com>
2015-10-02 10:51:10 -0400

    Changed cookie expiration date

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -2,7 +2,7 @@ class CookiesController < ApplicationController
 
   def set_school_id_cookie
     school_id = params[:school_id]
-    cookies[:school_id] = { value: school_id, expires: 9.months.from_now }
+    cookies[:school_id] = { value: school_id, expires: 2.months.from_now }
     if current_user
       current_user.update(school_id: school_id)
     end
