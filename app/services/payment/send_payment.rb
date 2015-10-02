ffb82283d5dfddfaf2afa1f7cc1f117c40df23e2
JT <jtjobe@gmail.com>
2015-10-02 11:28:40 -0400

    removed comments from PR

--- app/services/payment/send_payment.rb
+++ app/services/payment/send_payment.rb
@@ -3,13 +3,11 @@ class SendPayment
 
   def call
     if context.is_payment_required == true
-      puts "PAYMENT REQUIRED" # remove later, here for testing
       charge = context.charge
-      # processor = PaymentFactory.new.build
-      # payment = processor.send_charge(charge)
-      # context.payment = payment
+      processor = PaymentFactory.new.build
+      payment = processor.send_charge(charge)
+      context.payment = payment
     else
-      puts "NOOOOO PAYMENT REQUIRED" # remove later, here for testing
       return
     end
   end
