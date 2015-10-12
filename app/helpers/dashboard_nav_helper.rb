7f45dad2bb403e72385fa68578e3abc216f2934c
JT <jtjobe@gmail.com>
2015-10-12 18:11:32 -0400

    Fixed dashboard_nav helper bug

--- app/helpers/dashboard_nav_helper.rb
+++ app/helpers/dashboard_nav_helper.rb
@@ -8,35 +8,23 @@ module DashboardNavHelper
   def generate_dashboard_nav_links
     # user must be signed in to view dashbar pages
     return false unless user_signed_in?
-
-    all_dashboard_links = {
-      tutor: {
-        # name seen on the dashboard nav => where it links to
+    
+    if current_user.role.to_sym == :tutor
+      @dashboard_links = {
         'Home'      => home_tutor_path(@tutor.slug),
         'Schedule'  => schedule_tutor_path(@tutor.slug),
         'Courses'   => tutor_courses_path(@tutor.slug),
         'Promotions' => tutor_promotions_path(@tutor.slug),
         'Profile'   => profile_tutor_path(@tutor.slug),
         'Settings'  => account_tutor_path(@tutor.slug)
-      },
-
-      student: {
+      }
+    else
+      @dashboard_links = {
         'Home'          => home_student_path(@student.slug),
         'Settings'      => account_student_path(@student.slug),
         'Find a Tutor'  => search_path,
-      },
-
-      # admin: {
-      #   # 'Home'              => dashboard_link,
-      #   # 'Tutors'            => schedule_link,
-      #   # 'Reports'           => reports_link,
-      #   # 'Schools & Courses' => school_and_courses_link,
-      #   # 'Settings'          => settings_link,
-      # }
-    }
-
-    user_type = current_user.role.to_sym
-    @dashboard_links = all_dashboard_links[user_type]
+      }
+    end
 
     @dashboard_links.each do |name, link|
       @current = name if request.fullpath.start_with? link
