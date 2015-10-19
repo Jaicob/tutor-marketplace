42b6f2b26438a270100d888e3dab3aa761e4b6c8
JT <jtjobe@gmail.com>
2015-10-19 15:53:18 -0400

    PR review changes:

--- app/services/payment/process_payment.rb
+++ app/services/payment/process_payment.rb
@@ -8,17 +8,16 @@ end
 ##
 # Params for easy setup/testing in console
 
-# free_from_axon promo
-@promotion = Promotion.create(category: 5, amount: 10, valid_from: Date.today, valid_until: Date.today + 30, redemption_limit: 200, tutor_id: 23)
+# @promotion = Promotion.create(category: 5, amount: 10, valid_from: Date.today, valid_until: Date.today + 30, redemption_limit: 200, tutor_id: 23)
 
-params = {
-  tutor: Tutor.find(23),
-  appointments: [Appointment.first],
-  customer_id: Student.find(22).customer_id,
-  rates: [23],
-  transaction_percentage: 15.0,
-  promotion_id: @promotion.id,
-  is_payment_required: true
-}
+# params = {
+#   tutor: Tutor.find(23),
+#   appointments: [Appointment.first],
+#   customer_id: Student.find(22).customer_id,
+#   rates: [23],
+#   transaction_percentage: 15.0,
+#   promotion_id: @promotion.id,
+#   is_payment_required: true
+# }
 
 # When called in isolation for preview of charges during checkout, before final payment, it's OK to omit customer_id
