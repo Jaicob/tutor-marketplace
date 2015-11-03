12ea5f7640b1169aa8c9eb58a96ee278c26babf0
JT <jtjobe@gmail.com>
2015-11-03 10:59:41 -0500

    set default pic for campus bg when no school is set

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   before_action :set_user
   before_action :set_school
-  before_filter :set_tutor
+  before_action :set_tutor
   before_action :set_student
 
   helper DashboardNavHelper
