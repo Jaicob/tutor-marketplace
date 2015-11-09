196c33bace39e758f5bd54e7b790a4dc0d484bcc
JT <jtjobe@gmail.com>
2015-11-09 11:49:25 -0500

    removed accidentally added files

--- app/assets/images/Axon Tutors_files/TabBar.self-e3eb871183da82f59575329ddc1f0bf1cc34b130e924ba4c8661a67f9b7d5f9b.js
+++
@@ -1,7 +0,0 @@
-var TabBar = React.createClass({
-  displayName: "TabBar",
-
-  render: function () {
-    return React.createElement("div", { className: "appointment-selector__tab-bar" });
-  }
-});
