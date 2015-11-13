28949fd6d386f2c9aa1f89c0a91cf80254f16077
Jaicob <jaicob@icloud.com>
2015-11-13 03:37:28 -0500

    Configure redis and sidekiq shit to work for fucking finally dammut shit oh m g yassss

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,3 +16,24 @@ stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
 pid "#{app_dir}/tmp/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+
+  Sidekiq.configure_client do |config|
+    config.redis = { size: 1 }
+  end
+end
