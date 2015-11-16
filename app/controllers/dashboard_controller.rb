2a61143cdafa56bd10dde00bd947d514cd8bfe22
JT <jtjobe@gmail.com>
2015-11-15 21:21:19 -0500

    added styles to tutor application

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,21 +6,21 @@ class DashboardController < ApplicationController
 
   helper DashboardNavHelper
 
-  # def onboarding_check
-  #   if current_user.tutor && current_user.tutor.onboarding_status != 'Finished'
-  #     if @tutor.onboarding_status == 'Step 1'
-  #       redirect_to onboarding_application_tutor_path(@tutor.slug)
+  def onboarding_check
+    if current_user.tutor && current_user.tutor.onboarding_status != 'Finished'
+      if @tutor.onboarding_status == 'Step 1'
+        redirect_to onboarding_application_tutor_path(@tutor.slug)
 
-  #     elsif @tutor.onboarding_status == 'Step 2'
-  #       redirect_to onboarding_courses_tutor_path(@tutor.slug)
+      elsif @tutor.onboarding_status == 'Step 2'
+        redirect_to onboarding_courses_tutor_path(@tutor.slug)
 
-  #     elsif @tutor.onboarding_status == 'Step 3'
-  #       redirect_to onboarding_schedule_tutor_path(@tutor.slug)
+      elsif @tutor.onboarding_status == 'Step 3'
+        redirect_to onboarding_schedule_tutor_path(@tutor.slug)
 
-  #     elsif @tutor.onboarding_status == 'Step 4'
-  #       redirect_to onboarding_payment_details_tutor_path(@tutor.slug)
-  #     end
-  #   end
-  # end
+      elsif @tutor.onboarding_status == 'Step 4'
+        redirect_to onboarding_payment_details_tutor_path(@tutor.slug)
+      end
+    end
+  end
 
 end
