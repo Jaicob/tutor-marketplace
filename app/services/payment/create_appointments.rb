388459065324c7f4f959670af40537bb2f559ec5
JT <jtjobe@gmail.com>
2015-12-08 14:01:57 -0500

    created appointment interactor

--- app/services/payment/create_appointments.rb
+++ app/services/payment/create_appointments.rb
@@ -1,7 +1,28 @@
 class CreateAppointments
   include Interactor
 
+  # required_params
+    # :tutor_id
+    # :student_id
+    # :stripe_token
+    # :appts_info [{slot_id: x, course_id: x, start_time: xxx},{slot_id: x, course_id: x, start_time: xxx}]
+    # :promotion_id (optional)
+
   def call
+    context.appts = []
+
+    context.appts_info.each do |appt_info|
+      new_appt = Appointment.create(
+        student_id: context.student,
+        slot_id: appt_info['slot_id'],
+        course_id: appt_info['course_id'],
+        start_time: appt_info['start_time']
+      )
+      context.appts << new_appt
+    end
+
+    puts "CONTEXT.APPTS = #{context.appts}"
+
   end
 
 end
