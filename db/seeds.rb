a8b279dc63932dc43e71ad88deb6275d7b8aae72
Jaicob <jaicob@icloud.com>
2015-12-31 15:03:04 -0500

    Change email configs for development
    I changed our delivery method for emial to be test while in dev so that
    emails are not actually sent out. Additionally I changed all of our seed
    emails to use a safe sink email domain just incase.

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,7 +54,7 @@ end
   User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.safe_email(SecureRandom.hex(8).to_s),
+    email:  "#{SecureRandom.hex(8).to_s} @sink.sendgrid.net",,
     password: 'password',
     password_confirmation: 'password')
 }
