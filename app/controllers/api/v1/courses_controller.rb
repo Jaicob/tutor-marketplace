91bbeae8d7f312fc475172c40a15fa9a5a49b55a
JT <jtjobe@gmail.com>
2016-01-13 18:06:10 -0500

    back to normal with course dropdowns

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::CoursesController < API::V1::Defaults
 
   def index
-    @courses = CourseSearchPopulater.new(params[:school_id], params[:subject_id]).courses_with_active_tutors
+    @courses = School.find(params[:school_id]).courses_for_subject(params[:subject_id])
     respond_with(@courses)
   end
 
--- app/controllers/api/v1/subjects_controller.rb
+++ app/controllers/api/v1/subjects_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::SubjectsController < API::V1::Defaults
 
   def index
-    @subjects = CourseSearchPopulater.new(params[:school_id]).subjects_with_active_tutors
+    @subjects = School.find(params[:school_id]).subjects
     respond_with(@subjects)
   end
 
