fd5f7e3ae3433ee3cf00ab7694839f6746df22f9
JT <jtjobe@gmail.com>
2016-01-14 13:14:33 -0500

    mobile nav bar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # This is already defined in config/environment.rb
   #   config.action_mailer.smtp_settings = {
