b46ca8c4f3edafe3bf1d130247ace5bf54d6854f
JT <jtjobe@gmail.com>
2016-02-10 14:55:35 -0500

    working but weirdly

--- app/assets/javascripts/single_views/search.js
+++ app/assets/javascripts/single_views/search.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-	var updateReactComponents = function(){
-		var schoolId = $("#school").val();
-		var courseId = $("#course").val();
-		if ($.isNumeric(schoolId) && $.isNumeric(courseId)) {
-			React.render(
-			    React.createElement(TutorCardContainer, {
-			    	school: schoolId,
-			    	course: courseId
-			    }),
-			    document.querySelector('#tutorCardContainer')
-			);
-		} else {
-			swal("Uh-oh", "Please select a school and a course.", "error");
-		}
-	}
+// $(document).ready(function(){
+// 	var updateReactComponents = function(){
+// 		var schoolId = $("#school").val();
+// 		var courseId = $("#course").val();
+// 		if ($.isNumeric(schoolId) && $.isNumeric(courseId)) {
+// 			React.render(
+// 			    React.createElement(TutorCardContainer, {
+// 			    	school: schoolId,
+// 			    	course: courseId
+// 			    }),
+// 			    document.querySelector('#tutorCardContainer')
+// 			);
+// 		} else {
+// 			swal("Uh-oh", "Please select a school and a course.", "error");
+// 		}
+// 	}
 
-	$("#school")[0].selectedIndex = 0;
-	$("#subject")[0].selectedIndex = 0;
-	$("#course")[0].selectedIndex = 0;
+// 	$("#school")[0].selectedIndex = 0;
+// 	$("#subject")[0].selectedIndex = 0;
+// 	$("#course")[0].selectedIndex = 0;
 
-	$("#tutorSearchButton").on("click", updateReactComponents);
-});
+// 	$("#tutorSearchButton").on("click", updateReactComponents);
+// });
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,20 @@ class SearchController < ApplicationController
   respond_to :json
 
   def search_from_home
-    search_hash = {}
-    search_hash[:indirect_search] = true # this means a search that was not initiated from the main search page (i.e. from the Home page or the back button on a tutor's profile reached from search results)
-    search_hash[:course_id] = params[:course][:course_id]
+    # if request.referer == root_url
+      puts "request_referer_data_type = #{request.referer.class}"
+      puts "request.referer = #{request.referer}"
+      puts "CALLED!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts "CALLED!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      search_hash = {}
+      search_hash[:indirect_search] = true # true means that search not initiated from the main search page (i.e. from the Home page or the back button on a tutor's profile reached from search results)
+      search_hash[:course_id] = params[:course][:course_id]
+    # end
     redirect_to search_path(search_hash)
   end
 
 end
+
+
+#     | REQUEST REFERER: http://dockerhost:3000/search   !!!!!!!!!!!!
+# web_1     | REQUEST REFERER: http://dockerhost:3000/search   !!!!!!!!!!!!
