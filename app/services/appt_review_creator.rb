41c719f807ac43fb524608e581e1a9670fecba61
JT <jtjobe@gmail.com>
2016-02-25 12:39:13 -0500

    basic masks working

--- app/services/appt_review_creator.rb
+++ app/services/appt_review_creator.rb
@@ -12,8 +12,8 @@ class ApptReviewCreator
 
   # public method
   def reviews_needed?
-    false # this disables the review cards from appearing to students until we're ready to launch the review functionality
-    # get_appts_to_review.any? ? true : false
+    # false # this disables the review cards from appearing to students until we're ready to launch the review functionality
+    get_appts_to_review.any? ? true : false
   end
 
   # private method
