853101ed9ca898b235300125c39fb09f2ce7e2e7
JT <jtjobe@gmail.com>
2016-03-03 15:45:07 -0500

    changed instance variable typo in error mailer and chaged 100 to 300 everywhere

--- app/mailers/production_error_mailer.rb
+++ app/mailers/production_error_mailer.rb
@@ -2,7 +2,7 @@ class ProductionErrorMailer < ApplicationMailer
   default template_path: "mailers/#{self.name.underscore}"
 
   def send_error_report(error_report)
-    @error = error
+    @error_report = error_report
     mail(to: 'dev@axontutors.com', subject: "PRODUCTION ERROR: #{@error_report['error']}")
   end
 
