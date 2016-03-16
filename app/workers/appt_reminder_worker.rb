b9a6e3862ea13ec6296c84a0f668b153b0e87b11
JT <jtjobe@gmail.com>
2016-03-16 11:15:10 -0400

    added appt follow up email

--- app/workers/appt_reminder_worker.rb
+++ app/workers/appt_reminder_worker.rb
@@ -2,13 +2,13 @@ class ApptReminderWorker
   include Sidekiq::Worker 
 
   # This worker handles the appointment reminder emails. It only 
-  # sends a reminder email if the appt_reminder_email_date on an appointment
+  # sends a reminder email if the appt_reminder_email_time on an appointment
   # matches the current date, which effectively disables any out-of-date 
   # reminders from being sent if the appointment date was changed.
 
   def perform(appointment_id)
     @appt = Appointment.find(appointment_id)
-    @date = Time.at(@appt.appt_reminder_email_date)
+    @date = @appt.appt_reminder_email_time.to_date
     if @date == Date.today
       AppointmentMailer.appointment_reminder_for_tutor(appointment_id)
       AppointmentMailer.appointment_reminder_for_student(appointment_id)
