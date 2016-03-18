9c8b2c1be543d4aadb70073da1863545410608b8
JT <jtjobe@gmail.com>
2016-03-18 14:21:04 -0400

    edited error report mailer

--- app/mailers/production_error_mailer.rb
+++ app/mailers/production_error_mailer.rb
@@ -1,9 +1,10 @@
 class ProductionErrorMailer < ApplicationMailer
   default template_path: "mailers/#{self.name.underscore}"
 
-  def send_error_report(env, error_report)
+  def send_error_report(env_name, error_report)
+    @env_name = env_name
     @error_report = error_report
-    mail(to: 'dev@axontutors.com', subject: "#{env} ERROR: #{@error_report['error']}")
+    mail(to: 'dev@axontutors.com', subject: "#{env_name} ERROR: #{@error_report['error']}")
   end
 
 end
